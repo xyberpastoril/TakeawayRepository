be6397fc5822b9c01c734196f88805e7b9ce379d
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2022-02-12 19:50:11 +0800

    Implement Homepage Static UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,116 @@
 @extends('includes.template')
 
 @section('content')
-<h1>Test</h1>
+
+<div class="d-lg-flex justify-content-between mb-3">
+    <div class="">
+        <p class="mb-1 section-subtitle">
+            <i class="fa-solid fa-lightbulb text-warning"></i>
+            <b>Source of the Day</b>
+        </p>
+        <p class="section-title-with-subtitle m-0"><b>The quick brown fox jumps over the lazy fat dog</b></p>
+        <p>
+            <span class="text-muted">Feb. 12, 2022 | </span>
+            <span class="ml-3 badge rounded-pill bg-primary">Test</span>
+        </p>
+    </div>
+    <div class="mt-3">
+        <a href="#" class="btn btn-outline-primary">
+            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+            Read Full Source
+        </a>
+    </div>
+</div>
+
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4 mb-5">
+    <div class="col">
+        <div class="card card-hover h-100">
+            <div class="card-body">
+                <h5 class="card-title">Card card-hover title</h5>
+                <p class="card-text">This is a wider card card-hover with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card card-hover h-100">
+            <div class="card-body">
+                <h5 class="card-title">Card card-hover title</h5>
+                <p class="card-text">This card card-hover has supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card card-hover h-100">
+            <div class="card-body">
+                <h5 class="card-title">Card card-hover title</h5>
+                <p class="card-text">This is a wider card card-hover with supporting text below as a natural lead-in to additional
+                    content. This card card-hover has even longer content than the first to show that equal height action.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="d-lg-flex justify-content-lg-between mb-4">
+    <div class="">
+        <p class="section-title m-0">
+            <i class="fa-solid fa-rss text-primary"></i>
+            <b>Most Recent Sources</b>
+        </p>
+    </div>
+    <div class="mt-1">
+        <a href="#" class="btn btn-outline-primary">
+            <i class="fa-solid fa-angles-right"></i>
+            See More
+        </a>
+    </div>
+</div>
+
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 g-4">
+    <div class="col">
+        <div class="card card-hover h-100">
+            <div class="card-body">
+                <h5 class="card-title">Card card-hover title</h5>
+                <span class="badge rounded-pill bg-primary">Example Tag</span>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Feb. 12, 2022</small>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card card-hover h-100">
+            <div class="card-body">
+                <h5 class="card-title">Card card-hover title</h5>
+                <span class="badge rounded-pill bg-primary">Example Tag</span>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Feb. 12, 2022</small>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card card-hover h-100">
+            <div class="card-body">
+                <h5 class="card-title">Card card-hover title</h5>
+                <span class="badge rounded-pill bg-primary">Example Tag</span>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Feb. 12, 2022</small>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card card-hover h-100">
+            <div class="card-body">
+                <h5 class="card-title">Card card-hover title</h5>
+                <span class="badge rounded-pill bg-primary">Example Tag</span>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Feb. 12, 2022</small>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
