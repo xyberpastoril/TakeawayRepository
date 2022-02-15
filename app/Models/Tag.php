6096c7bf1cc6b8b5dfab7e23e8babb672c02975f
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-02-15 10:12:19 +0800

    Correct `Tag` Model to set `$primaryKey` to false

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     /**
+     * Indiciates if the model has a primary key.
+     * 
+     * @var bool
+     */
+    public $primaryKey = false;
+
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
