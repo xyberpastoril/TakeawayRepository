1753179e2f697b29d6e3f9ff057be43ee1a1fe1a
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-02-17 22:06:23 +0800

    Temporarily disable password reset routes
    This features requires sending emails for password reset links, which is not possible at the moment.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+// Authentication Routes
+Route::get('login', [\App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
+// Registration Routes...
+Route::get('register', [\App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [\App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
+// Password Reset Routes...
+// Route::get('password/reset', [\App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+// Route::post('password/email', [\App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+// Route::get('password/reset/{token}', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+// Route::post('password/reset', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
 
 Route::group(['middleware' => 'auth'], function() {
     Route::view('/', 'home')->name('home');
