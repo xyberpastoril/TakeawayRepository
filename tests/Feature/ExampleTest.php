a89c8deeedbba76d54db37322ca3753b9d21ab2a
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-02-17 22:09:15 +0800

    Reinstate `ExampleTest.php` in `\Tests\Feature`
    Removal of this file resulted to failures of `laravel-tests` GitHub action.

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Example test.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+}
