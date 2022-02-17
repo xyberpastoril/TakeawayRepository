f4bc91576a5908aa7466f8d589b43f67c9999ff4
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-02-17 20:42:48 +0800

    Refactor routes to use one-liner codes

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/source', function () {
-    return view('source.show');
-})->name('source.show');
-
-Route::get('/settings', function () {
-    return view('settings.index');
-})->name('settings');
+Route::view('/', 'home')->name('home');
+Route::view('/source', 'source.show')->name('source.show');
+Route::view('/settings', 'settings.index')->name('settings');
