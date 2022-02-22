103dbe3105cc1fc7393ce97f1df6054dedd45c4f
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-02-22 21:06:24 +0800

    Add test `test_update_source_unauthorized`

--- tests/Feature/SourceCRUDTest.php
+++ tests/Feature/SourceCRUDTest.php
@@ -38,6 +38,54 @@ class SourceCRUDTest extends TestCase
     }
 
     /**
+     * Test updating of source through PUT route if you're unauthorized.
+     *
+     * @return void
+     */
+    public function test_update_source_put_route_unauthorized()
+    {
+        // Create User
+        $user = \App\Models\User::create([
+            'email' => 'example@example.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('password'),
+        ]);
+
+        // Create Source
+        $response = $this->actingAs($user)
+            ->withHeaders([
+                'X-Header' => 'Value',
+            ])->post("/source", [
+                'title' => 'Example Title',
+                'reference_url' => 'https://google.com',
+                'date' => '2022-02-15',
+            ]);
+
+        $response->assertStatus(302);
+
+        $user->sources;
+        $source = $user->sources[0];
+
+        // Create Another User (who is unauthorized to touch newly created source)
+        $user2 = \App\Models\User::create([
+            'email' => 'example2@example.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('password'),
+        ]);
+
+        // Attempt User 2 to update specific source he's unauthorized to update
+        $response = $this->actingAs($user2)
+            ->withHeaders([
+                'X-Header' => 'Value',
+            ])->put("/source/{$source->uuid}", [
+                'title' => 'Test',
+                'reference_url' => 'https://www.google.com',
+                'date' => '2022-02-22',
+            ]);
+
+        $response->assertStatus(404);
+        $this->assertTrue(\App\Models\Source::count() > 0);
+    }
+
+    /**
      * Test storing of source through POST route with an invalid URL
      * for `reference_url` field.
      *
