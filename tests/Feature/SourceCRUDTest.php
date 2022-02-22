c81162cff8c822ab1d9d2e471d2aa96fc795ecbf
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-02-22 21:06:02 +0800

    Remove unnecessary tests

--- tests/Feature/SourceCRUDTest.php
+++ tests/Feature/SourceCRUDTest.php
@@ -16,11 +16,13 @@ class SourceCRUDTest extends TestCase
      */
     public function test_store_source_post_route()
     {
+        // Create User
         $user = \App\Models\User::create([
             'email' => 'example@example.com',
             'password' => \Illuminate\Support\Facades\Hash::make('password'),
         ]);
 
+        // Create Source
         $response = $this->actingAs($user)
             ->withHeaders([
                 'X-Header' => 'Value',
@@ -61,73 +63,4 @@ class SourceCRUDTest extends TestCase
         // $response->assertSessionHasErrors();
         // $this->assertFalse(\App\Models\Source::count() > 0);
     // }
-
-    /**
-     * Test updating of source through PUT route.
-     * 
-     * @return void 
-     */
-    public function test_update_source_put_route()
-    {
-        $user = \App\Models\User::create([
-            'email' => 'example@example.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('password'),
-        ]);
-
-        $user->sources()->create([
-            'title' => 'Example Title',
-            'reference_url' => 'https://google.com',
-            'date' => '2022-02-15',
-        ]);
-
-        $this->assertTrue(\App\Models\Source::count() > 0);
-
-        $response = $this->actingAs($user)
-            ->withHeaders([
-                'X-Header' => 'Value',
-            ])->put("/source/{$user->sources[0]->uuid}", [
-                'title' => 'Test 2',
-                'reference_url' => 'https://www.facebook.com',
-                'date' => '2022-02-14',
-            ]);
-
-        $response->assertStatus(302);
-        $response->assertSessionHasNoErrors();
-        $this->assertTrue(\App\Models\Source::count() > 0);
-
-        $source = \App\Models\Source::where('uuid', '=', $user->sources[0]->uuid)->first();
-
-        $this->assertTrue($source->title == "Test 2");
-    }
-
-    /**
-     * Test deleting a source through DELETE route.
-     * 
-     * @return void
-     */
-    public function test_delete_source_delete_route()
-    {
-        $user = \App\Models\User::create([
-            'email' => 'example@example.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('password'),
-        ]);
-
-        $user->sources()->create([
-            'title' => 'Example Title',
-            'reference_url' => 'https://google.com',
-            'date' => '2022-02-15',
-        ]);
-
-        $this->assertTrue(\App\Models\Source::count() > 0);
-
-        $response = $this->actingAs($user)
-            ->withHeaders([
-                'X-Header' => 'Value',
-            ])->delete("/source/{$user->sources[0]->uuid}");
-
-        $response->assertStatus(302);
-        $response->assertSessionHasNoErrors();
-        $this->assertFalse(\App\Models\Source::count() > 0);
-        $this->assertFalse(\App\Models\Tag::count() > 0);
-    }
 }
--- tests/Unit/RoutesTest.php
+++ tests/Unit/RoutesTest.php
@@ -27,24 +27,6 @@ class RoutesTest extends TestCase
     }
 
     /**
-     * Test whether the source page is accessible.
-     *
-     * @return void
-     */
-    public function test_source_page_access()
-    {
-        $user = \App\Models\User::create([
-            'email' => 'example@example.com',
-            'password' => \Illuminate\Support\Facades\Hash::make('password'),
-        ]);
-
-        $response = $this->actingAs($user)
-            ->get('/source');
-
-        $response->assertStatus(200);
-    }
-
-    /**
      * Test whether the settings page is accessible.
      *
      * @return void
